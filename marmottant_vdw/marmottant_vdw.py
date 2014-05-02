603265a5344bd9563c4ef2fef70cda2b493c2b17
Mark Harder <mark.harder899@gmail.com>
2014-05-02 10:16:03 -0400

    can create MarmottantVanDerWaal class

--- marmottant_vdw/marmottant_vdw.py
+++ marmottant_vdw/marmottant_vdw.py
@@ -1,3 +1,4 @@
+from numpy import array, size
 from scipy.interpolate import interp1d
 
 __version__ = '1.0'
@@ -6,13 +7,14 @@ class MarmottantVanDerWaal:
     def __init__(self, t, R, T, P, w, R0, Rbuck, Rrupt, Rbreak, KappaSh, Chi, SigmaR0, Rho, P0, SigmaL, C, Mu, KappaG, bubble_eq, gas):
         # Interpolate pulse
         Pac = interp1d(T, P)(t)
+        Rn = array([[0.001 for i in range(size(Pac))], [0.001 for i in range(len(Pac))]])
 
         if bubble_eq == "marm":
-            if R[0] < Rbuck:
+            if R[0][0] < Rbuck:
                 SigmaR = 0
-            elif R[0] >= Rbuck and R[0] < Rbreak:
-                SigmaR = Chi * ((R[0] / Rbuck) ** 2 - 1)
-            elif R[0] >= Rupt:
+            elif R[0][0] >= Rbuck and R[0] < Rbreak:
+                SigmaR = Chi * ((R[0][0] / Rbuck) ** 2 - 1)
+            elif R[0][0] >= Rupt:
                 SigmaR = SigmaL
             else:
                 SigmaR = SigmaL
@@ -20,7 +22,8 @@ class MarmottantVanDerWaal:
         if gas == "vdw":
             h = R0 / 5.6
             Rn[0] = R[1]
-            Rn[1] = (1/(Rho * w ** 2 * R0 ** 2 * R[0])) * ((-3 / 2 * Rho * (R0 * w * R[1]) ** 2) + (P0 + 2 * SigmaR0 / R0) * (((R[0] * R0) ** 3 - h ** 3) / (R0 ** 3 - h ** 3)) ** (-KappaG) * (1 - 3 * KappaG / C * (R0 * w * R[1]) * (R[0] * R0) ** 3 / ((R[0] * R0) ** 3 - h ** 3)) - 2 * SigmaR / (R0 * R[0]) - 4 * Mu * w * R[1] / R[0] - 4 * KappaSh * w * R[1] / (R0 * R[0] ** 2) - (P0 + P0 * Pac))
+
+            Rn[1] = (1/(Rho * w ** 2 * R0 ** 2 * R[0][0])) * ((-3 / 2 * Rho * (R0 * w * R[1][0]) ** 2) + (P0 + 2 * SigmaR0 / R0) * (((R[0][0] * R0) ** 3 - h ** 3) / (R0 ** 3 - h ** 3)) ** (-KappaG) * (1 - 3 * KappaG / C * (R0 * w * R[1][0]) * (R[0][0] * R0) ** 3 / ((R[0][0] * R0) ** 3 - h ** 3)) - 2 * SigmaR / (R0 * R[0][0]) - 4 * Mu * w * R[1][0] / R[0][0] - 4 * KappaSh * w * R[1][0] / (R0 * R[0][0] ** 2) - (P0 + P0 * Pac))
 
         Rn = Rn.flatten(1)
         print(Rn)
--- marmottant_vdw/test/test_marmottant_vdw.py
+++ marmottant_vdw/test/test_marmottant_vdw.py
@@ -1,13 +1,13 @@
 from nose.tools import *
 from marmottant_vdw.marmottant_vdw import MarmottantVanDerWaal
-from numpy import arange, zeros
+from numpy import arange, array, zeros, size
 from math import pi
 
 def test():
     # Simulation time
     t = arange(0, 5, 0.001) * (10 ** -6)
     T = t * 2 * pi * 2.8e6
-    P = zeros((1, len(t)))
+    P = zeros((1, size(t)))
     R0 = 2e-6
     Rbuck = 0.99
     SigmaL = 0.073
@@ -18,9 +18,18 @@ def test():
     Mu = 0.001
     KappaG = 1.06
     KappaSh = 2.4e-9
-    # R = zeros((len(t), ))
+    R = [
+            zeros(size(t)),
+            zeros(size([0.1e6])),
+            zeros(size([2e-6])),
+            zeros(size([2.8e6]))
+            ]
+    w = 2 * pi * 2.8e6
     Rho = 998
     P0 = 101325
     C = 1481
-    MarmottantVanDerWaal(t, R, T, P, w, R0, Rbuck, Rrupt, Rbreak, KappaSh, Chi, SigmaR0, Rho, P0, SigmaL, C, Mu, KappaG, "marm", "vdw")
+    R_pert_ini = 1
+    V0 = 0
+    Rini = array([R_pert_ini, V0]).reshape(-1, 1)
+    MarmottantVanDerWaal(t, Rini, T, P, w, R0, Rbuck, Rrupt, Rbreak, KappaSh, Chi, SigmaR0, Rho, P0, SigmaL, C, Mu, KappaG, "marm", "vdw")
     assert 1 == 1
