7bc5878e5e30dd4a5d02152114599180b2109812
Mark Harder <mark.harder899@gmail.com>
2014-05-01 22:40:00 -0400

    first translation for first test case
    attempt to simulate bubble_eq="marm" gas="vdw"
    missing value of R

--- marmottant_vdw/marmottant_vdw.py
+++ marmottant_vdw/marmottant_vdw.py
@@ -1,5 +1,26 @@
+from scipy.interpolate import interp1d
+
 __version__ = '1.0'
 
 class MarmottantVanDerWaal:
-    def __init__(self):
-        return None
+    def __init__(self, t, R, T, P, w, R0, Rbuck, Rrupt, Rbreak, KappaSh, Chi, SigmaR0, Rho, P0, SigmaL, C, Mu, KappaG, bubble_eq, gas):
+        # Interpolate pulse
+        Pac = interp1d(T, P)(t)
+
+        if bubble_eq == "marm":
+            if R[0] < Rbuck:
+                SigmaR = 0
+            elif R[0] >= Rbuck and R[0] < Rbreak:
+                SigmaR = Chi * ((R[0] / Rbuck) ** 2 - 1)
+            elif R[0] >= Rupt:
+                SigmaR = SigmaL
+            else:
+                SigmaR = SigmaL
+
+        if gas == "vdw":
+            h = R0 / 5.6
+            Rn[0] = R[1]
+            Rn[1] = (1/(Rho * w ** 2 * R0 ** 2 * R[0])) * ((-3 / 2 * Rho * (R0 * w * R[1]) ** 2) + (P0 + 2 * SigmaR0 / R0) * (((R[0] * R0) ** 3 - h ** 3) / (R0 ** 3 - h ** 3)) ** (-KappaG) * (1 - 3 * KappaG / C * (R0 * w * R[1]) * (R[0] * R0) ** 3 / ((R[0] * R0) ** 3 - h ** 3)) - 2 * SigmaR / (R0 * R[0]) - 4 * Mu * w * R[1] / R[0] - 4 * KappaSh * w * R[1] / (R0 * R[0] ** 2) - (P0 + P0 * Pac))
+
+        Rn = Rn.flatten(1)
+        print(Rn)
--- marmottant_vdw/test/test_marmottant_vdw.py
+++ marmottant_vdw/test/test_marmottant_vdw.py
@@ -1,5 +1,26 @@
 from nose.tools import *
 from marmottant_vdw.marmottant_vdw import MarmottantVanDerWaal
+from numpy import arange, zeros
+from math import pi
 
 def test():
+    # Simulation time
+    t = arange(0, 5, 0.001) * (10 ** -6)
+    T = t * 2 * pi * 2.8e6
+    P = zeros((1, len(t)))
+    R0 = 2e-6
+    Rbuck = 0.99
+    SigmaL = 0.073
+    Chi = 0.38
+    SigmaR0 = Chi*((1/Rbuck)**2-1)
+    Rrupt = Rbuck * (1 + SigmaL / Chi) ** (0.5)
+    Rbreak = Rrupt
+    Mu = 0.001
+    KappaG = 1.06
+    KappaSh = 2.4e-9
+    # R = zeros((len(t), ))
+    Rho = 998
+    P0 = 101325
+    C = 1481
+    MarmottantVanDerWaal(t, R, T, P, w, R0, Rbuck, Rrupt, Rbreak, KappaSh, Chi, SigmaR0, Rho, P0, SigmaL, C, Mu, KappaG, "marm", "vdw")
     assert 1 == 1
